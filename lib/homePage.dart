d02769e372d3823fb765da3875d2e94cc826ee93
Tom Perin <57326877+feykro@users.noreply.github.com>
2022-05-14 16:16:45 +0200

    Adding password protection for rooms

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,6 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 import 'gameRoom.dart';
 import 'globals.dart' as globals;
@@ -28,6 +28,20 @@ class _HomePageState extends State<HomePage> {
 
     final _roomNameController = TextEditingController();
     final _roomPasswordController = TextEditingController();
+
+    void joinRoom(Map rooms) {
+      DatabaseReference playerRef = ref.child('${rooms['key']}/players').push();
+      globals.userId = playerRef.key!;
+      playerRef.set(
+        {"name": globals.username, "isReady": false},
+      );
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => GameRoomPage(roomName: rooms['name'], roomId: rooms['key']),
+        ),
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -64,8 +78,7 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                         StatefulBuilder(
-                          builder:
-                              (BuildContext context, StateSetter stateSetter) {
+                          builder: (BuildContext context, StateSetter stateSetter) {
                             return Column(
                               children: [
                                 SwitchListTile(
@@ -74,13 +87,10 @@ class _HomePageState extends State<HomePage> {
                                   onChanged: (val) {
                                     stateSetter(() => lockedRoom = val);
                                   },
-                                  secondary: Icon(lockedRoom
-                                      ? Icons.lock
-                                      : Icons.lock_open),
+                                  secondary: Icon(lockedRoom ? Icons.lock : Icons.lock_open),
                                 ),
                                 Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 15.0),
+                                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
                                   child: Visibility(
                                     visible: lockedRoom,
                                     child: TextField(
@@ -108,16 +118,11 @@ class _HomePageState extends State<HomePage> {
                           child: ElevatedButton(
                               onPressed: () {
                                 //TODO: create the room and go in it
-                                print(
-                                    "Salle ${_roomNameController.text} créée. Is locked = ${lockedRoom}");
+                                print("Salle ${_roomNameController.text} créée. Is locked = ${lockedRoom}");
                                 if (lockedRoom) {
-                                  print(
-                                      "Mot de passe : ${_roomPasswordController.text}");
+                                  print("Mot de passe : ${_roomPasswordController.text}");
                                 }
-                                GameRoom room = GameRoom(
-                                    _roomNameController.text,
-                                    globals.username,
-                                    lockedRoom,
+                                GameRoom room = GameRoom(_roomNameController.text, globals.username, lockedRoom,
                                     _roomPasswordController.text);
                                 createRoom(room);
                                 setState(() {
@@ -138,8 +143,7 @@ class _HomePageState extends State<HomePage> {
           Flexible(
             child: FirebaseAnimatedList(
                 query: ref,
-                itemBuilder: (BuildContext context, DataSnapshot snapshot_,
-                    Animation<double> animation, int index) {
+                itemBuilder: (BuildContext context, DataSnapshot snapshot_, Animation<double> animation, int index) {
                   Map rooms = snapshot_.value as Map;
                   rooms['key'] = snapshot_.key;
                   return FutureBuilder<DataSnapshot>(
@@ -149,9 +153,7 @@ class _HomePageState extends State<HomePage> {
                           mainAxisSize: MainAxisSize.min,
                           children: <Widget>[
                             ListTile(
-                              leading: Icon(rooms['hasPassword']
-                                  ? Icons.lock
-                                  : Icons.lock_open),
+                              leading: Icon(rooms['hasPassword'] ? Icons.lock : Icons.lock_open),
                               title: Text(rooms['name']),
                               subtitle: Text("owner : ${rooms['owner']}"),
                             ),
@@ -159,29 +161,20 @@ class _HomePageState extends State<HomePage> {
                               mainAxisAlignment: MainAxisAlignment.end,
                               children: <Widget>[
                                 Padding(
-                                  padding: const EdgeInsets.only(
-                                      bottom: 10.0, right: 10.0),
+                                  padding: const EdgeInsets.only(bottom: 10.0, right: 10.0),
                                   child: TextButton(
                                     child: const Text('Join room'),
                                     onPressed: () {
-                                      DatabaseReference playerRef = ref
-                                          .child('${rooms['key']}/players')
-                                          .push();
-                                      globals.userId = playerRef.key!;
-                                      playerRef.set(
-                                        {
-                                          "name": globals.username,
-                                          "isReady": false
-                                        },
-                                      );
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => GameRoomPage(
-                                              roomName: rooms['name'],
-                                              roomId: rooms['key']),
-                                        ),
-                                      );
+                                      if (rooms['hasPassword'] as bool) {
+                                        // display modal
+                                        showModalBottomSheet(
+                                            context: context,
+                                            builder: (BuildContext context) {
+                                              return buildModalScaffold(rooms, joinRoom);
+                                            });
+                                      } else {
+                                        joinRoom(rooms);
+                                      }
                                     },
                                   ),
                                 ),
@@ -198,15 +191,69 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-}
 
-/*
-                      builder: (context, snapshot) => RoomCard(
-                          roomList[index].name,
-                          roomList[index].owner,
-                          roomList[index].hasPassword,
-                          roomList[index].password));
-*/
+  Widget buildModalScaffold(Map rooms, Function joinRoom) {
+    var _textEditingController = TextEditingController();
+
+    var isPasswordOk = false;
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text("Please enter password"),
+          leading: IconButton(
+            icon: const Icon(Icons.cancel_outlined),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: TextField(
+              obscureText: true,
+              controller: _textEditingController,
+              maxLines: 1,
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+          ),
+          // Bouton pour valider modal et naviguer vers la salle en question
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: ElevatedButton(
+                onPressed: () {
+                  if (rooms["password"] as String == _textEditingController.text) {
+                    Navigator.of(context).pop();
+                    joinRoom(rooms);
+                  } else {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text("Erreur"),
+                            content: Text("Le mot de passe est incorrect."),
+                            actions: [
+                              TextButton(
+                                child: const Text("ok"),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        });
+                  }
+                },
+                child: const Text("Rejoindre")),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class GameRoom {
   String name;
@@ -217,12 +264,7 @@ class GameRoom {
   GameRoom(this.name, this.owner, this.hasPassword, [this.password]);
 
   Object? toJson() {
-    return {
-      "name": name,
-      "owner": owner,
-      "hasPassword": hasPassword,
-      if (hasPassword) "password": password
-    };
+    return {"name": name, "owner": owner, "hasPassword": hasPassword, if (hasPassword) "password": password};
   }
 }
 
@@ -232,9 +274,7 @@ class RoomCard extends StatelessWidget {
   bool hasPassword;
   String? password;
 
-  RoomCard(this.roomName, this.ownerName, this.hasPassword, this.password,
-      {Key? key})
-      : super(key: key);
+  RoomCard(this.roomName, this.ownerName, this.hasPassword, this.password, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -258,8 +298,7 @@ class RoomCard extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) =>
-                            GameRoomPage(roomName: roomName, roomId: ''),
+                        builder: (context) => GameRoomPage(roomName: roomName, roomId: ''),
                       ),
                     );
                   },
