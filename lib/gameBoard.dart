e609c84aefbe4f0fc3f799d660dd06b35da201b0
FlutLab.io <>
2022-06-30 12:51:36 +0000

    Switching to branch okay___

--- lib/gameBoard.dart
+++ lib/gameBoard.dart
@@ -80,7 +80,7 @@ class _GameBoardPageState extends State<GameBoardPage> {
             // Check qui win le tour, lui donner le point et le désigner en startPlayerIndex
             colorForTurn = '';
             turn += 1;
-            Future.delayed(Duration(seconds: 2), () {
+            Future.delayed(Duration(seconds: 3), () {
               setState(() {
                 playedCards = [];
               });
