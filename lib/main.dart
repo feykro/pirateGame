1678f8f7e8d7f3b9b99c26af0e27232a545ebf97
FlutLab.io <>
2022-08-03 12:32:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:pirate_app/GamePage.dart';
 import 'package:pirate_app/homePage.dart';
-import 'package:flutter/services.dart';
 
 import 'firebase_options.dart';
 import 'gameRoom.dart';
@@ -11,11 +10,6 @@ import 'globals.dart' as globals;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
-  ]).then((value) => runApp(MyApp()));
-  runApp(MyApp());
   start();
 }
 
@@ -66,6 +60,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  /*
   @override
   Widget build(BuildContext context) {
     final _inputController = TextEditingController();
@@ -130,6 +125,162 @@ class _LoginPageState extends State<LoginPage> {
                 },
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }*/
+  @override
+  Widget build(BuildContext context) {
+    final _inputController = TextEditingController();
+    return Scaffold(
+      backgroundColor: Color(0xFF14181B),
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 1,
+          decoration: BoxDecoration(
+            color: Color(0xFF14181B),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: Image.network(
+                'https://cf.geekdo-images.com/jiL3MIGH_w3g6El3OHVVig__opengraph/img/GI-QYAT26x67jcGYQkx79BgJrgw=/fit-in/1200x630/filters:strip_icc()/pic6137456.png',
+              ).image,
+            ),
+          ),
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Color(0x990F1113),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: Image.network(
+                  '',
+                ).image,
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 200, 0, 0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: DefaultTabController(
+                      length: 1,
+                      initialIndex: 0,
+                      child: Column(
+                        children: [
+                          TabBar(
+                            isScrollable: true,
+                            labelColor: Colors.white,
+                            labelStyle: TextStyle(
+                              fontFamily: 'Outfit',
+                              color: Color(0xFF0F1113),
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            indicatorColor: Colors.white,
+                            tabs: [
+                              Tab(
+                                text: 'Sign In',
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: TabBarView(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsetsDirectional.fromSTEB(44, 0, 44, 0),
+                                  child: SingleChildScrollView(
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        Padding(
+                                          padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                                          child: TextFormField(
+                                            controller: _inputController,
+                                            obscureText: false,
+                                            decoration: InputDecoration(
+                                              labelText: 'Email Address',
+                                              labelStyle: TextStyle(
+                                                fontFamily: 'Lexend Deca',
+                                                color: Color(0xFF95A1AC),
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.normal,
+                                              ),
+                                              hintText: 'Enter your email...',
+                                              hintStyle: TextStyle(
+                                                fontFamily: 'Lexend Deca',
+                                                color: Color(0xFF95A1AC),
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.normal,
+                                              ),
+                                              enabledBorder: UnderlineInputBorder(
+                                                borderSide: BorderSide(
+                                                  color: Color(0x00000000),
+                                                  width: 1,
+                                                ),
+                                                borderRadius: BorderRadius.circular(8),
+                                              ),
+                                              focusedBorder: UnderlineInputBorder(
+                                                borderSide: BorderSide(
+                                                  color: Color(0x00000000),
+                                                  width: 1,
+                                                ),
+                                                borderRadius: BorderRadius.circular(8),
+                                              ),
+                                              filled: true,
+                                              fillColor: Colors.white,
+                                              contentPadding: EdgeInsetsDirectional.fromSTEB(20, 24, 20, 24),
+                                              prefixIcon: Icon(
+                                                Icons.tag,
+                                              ),
+                                            ),
+                                            style: TextStyle(
+                                              fontFamily: 'Outfit',
+                                              color: Color(0xFF0F1113),
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.normal,
+                                            ),
+                                          ),
+                                        ),
+                                        ValueListenableBuilder<TextEditingValue>(
+                                          valueListenable: _inputController,
+                                          builder: (context, value, child) {
+                                            return Padding(
+                                                padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+                                                child: ElevatedButton(
+                                                  child: Text('Login'),
+                                                  style: ElevatedButton.styleFrom(
+                                                    primary: Colors.teal,
+                                                    onPrimary: Colors.white,
+                                                    shape: const BeveledRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                                                  ),
+                                                  onPressed: value.text.isNotEmpty
+                                                      ? () {
+                                                          globals.username = _inputController.text;
+                                                          Navigator.pushNamed(context, '/home');
+                                                        }
+                                                      : null,
+                                                ));
+                                          },
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
