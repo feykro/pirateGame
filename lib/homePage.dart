afe1086866666f37a5da5eceb55303807122cbe1
FlutLab.io <>
2022-08-04 12:37:46 +0000

    Autosave

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -186,7 +186,7 @@ class _HomePageState extends State<HomePage> {
                             decoration: BoxDecoration(
                               image: DecorationImage(
                                 colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
-                                image: Image.network("https://m.media-amazon.com/images/S/aplus-media-library-service-media/729e029c-1a4b-4cd4-a04f-c0b815c9868f.__CR0,0,970,300_PT0_SX970_V1___.png").image,
+                                image: Image.network("https://m.media-amazon.com/images/I/816nYpeEKLL.jpg").image,
                                 fit: BoxFit.cover,
                                 alignment: Alignment.topCenter,
                               ),
