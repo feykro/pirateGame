ec36cceea6184ef7a4a56cddeb67e63e088b00fd
FlutLab.io <>
2022-08-04 12:30:34 +0000

    Autosave

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -185,7 +185,7 @@ class _HomePageState extends State<HomePage> {
                         child: Container(
                             decoration: BoxDecoration(
                               image: DecorationImage(
-                                image: Image.network("https://cdn.shopify.com/s/files/1/0565/3230/4053/products/skull-king-family-games-grandpa-beck_s-games-united-states-product-gallery-_6_1200x775.jpg?v=1651003672", color: Colors.white.withOpacity(0.2), colorBlendMode: BlendMode.modulate).image,
+                                image: Image.network("https://cdn.shopify.com/s/files/1/0565/3230/4053/products/skull-king-family-games-grandpa-beck_s-games-united-states-product-gallery-_6_1200x775.jpg?v=1651003672", color: Colors.white.withOpacity(0.8), colorBlendMode: BlendMode.modulate).image,
                                 fit: BoxFit.fitWidth,
                                 alignment: Alignment.topCenter,
                               ),
